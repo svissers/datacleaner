28b7ae9be34efa3990ed47ee27b3c017c3bae082
IgorSchittekat <igor.schittekat@gmail.com>
2018-03-05 16:13:35 +0100

    added tests for edit_profile

--- src/tests.py
+++ src/tests.py
@@ -26,8 +26,7 @@ class DBManagerTests(TestCase):
 
     def test_create_user_success(self):
         db_manager.create_user('', '', '', 'test', 'test', 'test')
-        self.assertTrue(
-            db_manager.Account.query.filter_by(username='test').first())
+        self.assertTrue(db_manager.Account.query.filter_by(username='test').first())
 
     def test_create_user_email_already_used(self):
         db_manager.create_user('', '', '', 'used', 'notused1', 'test')
@@ -54,6 +53,51 @@ class DBManagerTests(TestCase):
     def test_validate_login_credentials_wrong_password(self):
         db_manager.create_user('', '', '', 'test', 'test', 'test')
         self.assertFalse(db_manager.validate_login_credentials('test', 'oops'))
+
+    def test_get_user(self):
+        db_manager.create_user('', '', '', 'test', 'test', 'test')
+        self.assertTrue(db_manager.get_user('test'))
+        self.assertFalse(db_manager.get_user('test2'))
+        self.assertTrue(db_manager.get_user('test').email == 'test')
+
+    def test_get_user_by_id(self):
+        db_manager.create_user('', '', '', 'test', 'test', 'test')
+        userid = db_manager.get_user('test').id
+        self.assertTrue(db_manager.get_user_by_id(userid))
+        self.assertFalse(db_manager.get_user_by_id(userid + 1))
+
+    def test_edit_user_success(self):
+        db_manager.create_user('test', 'test', '', 'test', 'test', 'test')
+        userid = db_manager.get_user('test').id
+        db_manager.edit_user(userid, 'test2', 'test2', 'test2', 'test2', 'test2', 'test2')
+        self.assertTrue(db_manager.get_user('test2'))
+        self.assertFalse(db_manager.get_user('test'))
+        db_manager.edit_user(userid, '', '', '', 'test3', '', '')
+        self.assertTrue(db_manager.get_user('test2').email == 'test3')
+
+    def test_edit_user_no_changes(self):
+        db_manager.create_user('test', 'test', 'test', 'test', 'test', 'test')
+        userid = db_manager.get_user('test').id
+        db_manager.edit_user(userid, '', '', '', '', '', '')
+        self.assertTrue(db_manager.get_user_by_id(userid).username == 'test')
+        self.assertTrue(db_manager.get_user_by_id(userid).email == 'test')
+        self.assertTrue(db_manager.get_user_by_id(userid).first_name == 'test')
+        self.assertTrue(db_manager.get_user_by_id(userid).last_name == 'test')
+        self.assertTrue(db_manager.get_user_by_id(userid).organization == 'test')
+
+    def test_edit_user_username_already_used(self):
+        db_manager.create_user('', '', '', 'test', 'test', 'test')
+        db_manager.create_user('', '', '', 'test2', 'test2', 'test2')
+        with self.assertRaises(Exception):
+            userid = db_manager.get_user('test').id
+            db_manager.edit_user(userid, '', '', '', '', 'test2', '')
+
+    def test_edit_user_email_already_used(self):
+        db_manager.create_user('', '', '', 'test', 'test', 'test')
+        db_manager.create_user('', '', '', 'test2', 'test2', 'test2')
+        with self.assertRaises(Exception):
+            userid = db_manager.get_user('test').id
+            db_manager.edit_user(userid, '', '', '', 'test2', '', '')
 
 
 if __name__ == '__main__':
