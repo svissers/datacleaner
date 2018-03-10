894be8f1c9bdbffef5f6374e57771048d97083a4
IgorSchittekat <igor.schittekat@gmail.com>
2018-03-10 23:07:22 +0100

    setup datatest

--- tests.py
+++ tests.py
@@ -2,11 +2,12 @@ from app import app
 from flask_testing import TestCase
 import unittest
 from app._user.models import User, db
+from app._data.models import Project
 
 
 # Test cases for _user ################################################
 
-class DBManagerTests(TestCase):
+class UserTests(TestCase):
     def create_app(self):
         return app
 
@@ -91,6 +92,29 @@ class DBManagerTests(TestCase):
         with self.assertRaises(Exception):
             userid = User.get_by_name('test').id
             User.update_by_id(userid, '', '', '', 'test2', '', '')
+
+    def test_init_admin(self):
+        self.assertFalse(User.get_by_name('admin'))
+        User.init_admin()
+        self.assertTrue(User.get_by_name('admin'))
+
+
+# Test cases for _data ################################################
+
+class DataTests(TestCase):
+    def create_app(self):
+        return app
+
+    def setUp(self):
+        db.create_all(app=app)
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_create_project_success(self):
+        Project('test', 'test').add_to_database()
+        self.assertTrue(Project.query.filter_by(name='test').first())
 
 
 if __name__ == '__main__':
