69267521924b3841e9d153c0f3382f2cb586bd7f
Andrei Bondarenko <andrei.i.bondarenko@gmail.com>
2018-03-24 20:56:45 +0100

    Updated controllers

--- app/User/controllers.py
+++ app/User/controllers.py
@@ -1,8 +1,32 @@
-from app import login_manager as lm, database as db
+from app import login_manager as lm
+from .forms import (
+    SignUpForm,
+    LoginForm,
+    EditForm
+)
+from flask import (
+    Blueprint,
+    render_template,
+    flash,
+    redirect,
+    url_for,
+    jsonify,
+    request
+)
+from flask_login import (
+    login_user,
+    logout_user,
+    login_required,
+    current_user
+)
+from .operations import (
+    get_user_with_id,
+    get_user_with_username,
+    create_user,
+    validate_login_credentials,
+    update_user_with_id
+)
 from .models import User
-from .forms import SignUpForm, LoginForm, EditForm
-from flask import Blueprint, render_template, flash, redirect, url_for, jsonify, request
-from flask_login import login_user, logout_user, login_required, current_user
 
 _user = Blueprint('user_bp', __name__, url_prefix='/user')
 lm.login_view = 'user_bp.login'
@@ -10,7 +34,7 @@ lm.login_view = 'user_bp.login'
 
 @lm.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return get_user_with_id(user_id)
 
 
 @_user.route('/autocomplete', methods=['GET'])
@@ -24,18 +48,16 @@ def signup():
     form = SignUpForm()
     if form.validate_on_submit():
         try:
-            new_user = User(
+            create_user(
                 form.first_name.data,
                 form.last_name.data,
-                form.organization.data,
                 form.email.data,
                 form.username.data,
                 form.password.data
             )
-            new_user.add_to_database()
             flash('You have been registered and can now log in.', 'success')
             return redirect(url_for('user_bp.login'))
-        except Exception as error:
+        except RuntimeError as error:
             flash(str(error), 'danger')
     return render_template('_user/signup.html', form=form)
 
@@ -49,7 +71,7 @@ def login():
         result = User.validate_login_credentials(form.username.data,
                                                  form.password.data)
         if result[0] is True:
-            user = User.get_by_name(form.username.data)
+            user = get_user_with_username(form.username.data)
             login_user(user)
             return redirect(url_for('main_bp.dashboard'))
         else:
@@ -67,23 +89,23 @@ def logout():
 @_user.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
-    form = EditForm(request.form)
+    form = EditForm()
     if form.validate_on_submit():
-        if not User.validate_login_credentials(current_user.username,
-                                               form.current_password.data)[0]:
+        if not validate_login_credentials(
+                current_user.username,
+                form.current_password.data)[0]:
             flash('Current password is not correct.', 'danger')
         try:
-            User.update_by_id(
+            update_user_with_id(
                 current_user.id,
                 form.first_name.data,
                 form.last_name.data,
-                form.organization.data,
                 form.email.data,
                 form.username.data,
                 form.new_password.data
             )
             flash('Your account information has been updated.', 'success')
-        except Exception as error:
+        except RuntimeError as error:
             flash(str(error), 'danger')
     return render_template('_user/profile.html', form=form)
 
