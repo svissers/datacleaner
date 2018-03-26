5659baddc557a86abfe02b047f3cece1142bac30
IgorSchittekat <igor.schittekat@gmail.com>
2018-03-26 14:41:26 +0200

    Fixed bug with capital letters for changing type of columns

--- app/Data/Transform/operations.py
+++ app/Data/Transform/operations.py
@@ -19,30 +19,31 @@ def change_attribute_type(table_name, table_col, new_type):
     try:
         if new_type == 'INTEGER':
             db.engine.execute(
-                'ALTER TABLE {0} ALTER COLUMN {1} TYPE INTEGER USING {1}::integer'.format(table_name, table_col))
+                'ALTER TABLE {0} ALTER COLUMN "{1}" TYPE INTEGER USING "{1}"::integer'.format(table_name, table_col))
         if new_type == 'BIGINT':
             db.engine.execute(
-                'ALTER TABLE {0} ALTER COLUMN {1} TYPE BIGINT USING {1}::bigint'.format(table_name, table_col))
+                'ALTER TABLE {0} ALTER COLUMN "{1}" TYPE BIGINT USING "{1}"::bigint'.format(table_name, table_col))
         if new_type == 'VARCHAR(10)':
             db.engine.execute(
-                'ALTER TABLE {0} ALTER COLUMN {1} TYPE VARCHAR(10)' .format(table_name, table_col))
+                'ALTER TABLE {0} ALTER COLUMN "{1}" TYPE VARCHAR(10)'.format(table_name, table_col))
         if new_type == 'VARCHAR(25)':
             db.engine.execute(
-                'ALTER TABLE {0} ALTER COLUMN {1} TYPE VARCHAR(25)' .format(table_name, table_col))
+                'ALTER TABLE {0} ALTER COLUMN "{1}" TYPE VARCHAR(25)'.format(table_name, table_col))
         if new_type == 'VARCHAR(255)':
             db.engine.execute(
-                'ALTER TABLE {0} ALTER COLUMN {1} TYPE VARCHAR(255)' .format(table_name, table_col))
+                'ALTER TABLE {0} ALTER COLUMN "{1}" TYPE VARCHAR(255)'.format(table_name, table_col))
         if new_type == 'TEXT':
             db.engine.execute(
-                'ALTER TABLE {0} ALTER COLUMN {1} TYPE TEXT'.format(table_name, table_col))
+                'ALTER TABLE {0} ALTER COLUMN "{1}" TYPE TEXT'.format(table_name, table_col))
         if new_type == 'BOOLEAN':
             db.engine.execute(
-                'ALTER TABLE {0} ALTER COLUMN {1} TYPE BOOLEAN USING {1}::boolean'.format(table_name, table_col))
+                'ALTER TABLE {0} ALTER COLUMN "{1}" TYPE BOOLEAN USING "{1}"::boolean'.format(table_name, table_col))
         if new_type == 'DATE':
             db.engine.execute(
-                "ALTER TABLE {0} ALTER COLUMN {1} TYPE DATE USING to_date({1}, 'YYYY-MM-DD')".format(table_name, table_col))
-    except Exception:
-        print("ERROR")
+                'ALTER TABLE {0} ALTER COLUMN "{1}" TYPE DATE USING to_date("{1}", \'YYYY-MM-DD\')'.
+                format(table_name, table_col))
+    except Exception as error:
+        print(str(error))
 
 
 def drop_attribute(table_name, attr):
