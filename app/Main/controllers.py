3a876e2451e7339f056011bbc1466bc4827e741c
Andrei Bondarenko <andrei.i.bondarenko@gmail.com>
2018-04-04 02:33:38 +0200

    Dashboard finished

--- app/Main/controllers.py
+++ app/Main/controllers.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
-# from app.Data.helpers import get_datasets, get_projects
 from app.Data.Import.forms import UploadForm
+from app.User.forms import EditForm
 from app.Project.forms import ProjectForm, ShareForm
 from app.Project.operations import get_all_projects_for_user
-from app.Data.operations import get_datasets
 
 _main = Blueprint('main_bp', __name__)
 
@@ -20,17 +19,17 @@ def index():
 @login_required
 def dashboard():
     projects = get_all_projects_for_user(current_user.id)
-    datasets = get_datasets(current_user.id)
 
     upload_form = UploadForm()
     project_form = ProjectForm()
     share_form = ShareForm()
+    edit_form = EditForm()
 
     return render_template(
         'Main/dashboard.html',
         projects=projects,
-        datasets=datasets,
         upload_form=upload_form,
         project_form=project_form,
-        share_form=share_form
+        share_form=share_form,
+        edit_form=edit_form
     )
