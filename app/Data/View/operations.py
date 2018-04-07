c528638abc4dfbf1f48515e45338e85d82a80278
Andrei Bondarenko <andrei.i.bondarenko@gmail.com>
2018-04-07 13:01:11 +0200

    Removed index column from csv export

--- app/Data/View/operations.py
+++ app/Data/View/operations.py
@@ -5,6 +5,7 @@ import pandas as pd
 def export_csv(table_name, delim=',', quote='"', null=''):
     try:
         df = pd.read_sql_table(table_name, db.engine)
+        df.drop(labels='index')
         df.fillna(null)
         return df.to_csv(sep=delim, quotechar=quote)
     except:
